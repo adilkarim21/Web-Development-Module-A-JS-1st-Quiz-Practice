47c67e53bdb60050760efc0cbfea62780cb6bb98
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-09-28 00:45:40 +0500

    practice done successfully.

---
+++ app.js
@@ -0,0 +1,265 @@
+// JS QUIZ PRACTICE
+
+
+// 1:
+
+// alert("Thanks for your input!");
+// alert( "Thanks for your input!" );
+
+
+// 2:
+
+// var name = "Adil";
+// var city = "karachi";
+
+// var nationality;
+// nationality = "PK";
+
+// alert("Thanks for your input!");
+
+// var thanx = "Thanks for your input!";
+// alert(thanx);
+
+
+// 3:
+
+// var weight = 150;
+// weight + 25;
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+
+
+// 5:
+
+// var popularNum = 4;
+// var popularNum = 2 + 2;
+
+// alert(2 + 2);
+
+// var popularNum = 12 - 24;
+
+// var popularNum = 4 * 5;
+
+// var num = 20;
+// var popularNum = num + 100;
+
+// var num1 = 73;
+// var num2 = 45;
+// var sum = num1 + num2;
+
+// var whatsLeftOver = 10 % 3;
+
+
+// 6:
+
+// var num = 2;
+// num = num + 1;
+// num++
+
+// var num = 2;
+// num = num  - 1;
+// num--
+
+// var num = 2;
+// num = num + 1;
+// ++num
+
+// var num = 2;
+// num = num  - 1;
+// --num
+
+
+// 7:
+
+// var total = 7 + 2 * 5;
+
+// var total = 7 = ( 2 * 5 );
+
+// var total = ( 7 + 2 ) * 5;
+
+// var total = ( 5 + 8 ) * ( 2 + 6 );
+
+// var total = ( ( 7 + 4 ) * 2 ) - 8;
+
+
+// 8:
+
+// alert("Thanks for your input!");
+
+// var message = "Thanks for your input!";
+// alert(message);
+
+// var userName = "Ahmed";
+// alert("Thanks, " + userName + "!");
+
+// var userName = "Ahmed ";
+// var message = "Thanks, ";
+// var banger = "!";
+// alert(message + userName + banger);
+
+// var userName = "Ahmed ";
+// var message = "Thanks, ";
+// var banger = "!";
+// var customerMess = message + userName + banger;
+// alert(customerMess);
+
+// alert("2" + "2");
+
+// alert("2 plus 2 equals " + 2 + 2);
+
+
+// 9:
+
+// var specs = prompt("Your species?" , "human");
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var specs = prompt(question , defaultAnswer);
+
+
+// 10:
+
+// var x = prompt("Where does the Tom lived?");
+// if ( x === "Vatican" ) {
+//     alert("Correct!");
+// }
+
+
+// 11:
+
+// var yourTicketNumber;
+// if ( yourTicketNumber !== 73850 ) {
+//     alert("Better luck next time.");
+// }
+
+// if ( 1 > 0 ) {
+// if ( 0 < 1 ) {
+// if ( 1 >= 1 ) {
+// if ( 0 <= 1 ) {
+// if ( 0 <= 1 ) {
+
+
+// 12:
+
+// var x = prompt("Where does the Peter lived.");
+// if ( x === "Vatican" ) {
+//     alert("Correct!");
+// }
+// else {
+//     alert("Wrong Answer!");
+// }
+
+
+// 13:
+
+// var weight = prompt("Enter your weight?");
+// var time = +prompt("Enter time.");
+// if ( weight > 80 && time > 6 ) {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Come to our cookout!");
+// }
+
+// var weight = prompt("Enter your weight?");
+// var time = +prompt("Enter time.");
+// var age = +prompt("Enter your age.");
+// var gender = prompt("Enter your gender.");
+// if ( weight < 80 && time > 6 && age > 17 && gender === "male" ) {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Come to our cookout!");
+// }
+
+
+// 14:
+
+// var a , b , c , d , e , f , g , h , x , y;
+// if ( ( x === y || a === b ) && c === d ) {
+//     g = h;
+// }
+// else {
+//     e = f;
+// }
+
+
+// 15:
+
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+
+// alert("Welcome to " + city3);
+
+// var cities = ["Atlanta","Baltimore","Chicago","Denver","Los Angeles","Seattle"];
+
+// alert("Welcome to " + cities[3]);
+
+// var mixedArr = [1 , "Bob" , true , undefined];
+
+
+// 16:
+
+// var pets = [];
+
+// 1 pets[0] = "cat";
+// 1 pets[1] = "dog";
+// 1 pets[2] = "bird";
+
+// pets.pop();
+
+// pets.push("fish","ferret");
+
+
+// 17:
+
+// pets.unshift("fish","ferret");
+
+// pets.shift();
+
+// pets.splice(2,2,"pig","duck","emu");
+
+// pets.splice(2,0,"pig","duck","emu");
+
+// pets.splice(2,2);
+
+
+// 18:
+
+// var cleanestCities = ["China","USA","India","Bangladesh","Pakistan"];
+
+// var cityToCheck = prompt("Enter one of the most cleanest city:");
+
+// for ( var i = 0 ; i <= cleanestCities.length ; i++ ) {
+//     if ( cityToCheck === cleanestCities[i] ) {
+//         alert("It's one of the most cleanest city in the whole mighty world!");
+//     }    
+// }
+
+
+// 20:
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+// var fullNames = [];
+// for ( var i = 0 ; i < firstNames.length ; i++ ) {
+//     for ( var j = 0 ; j < lastNames.length ; j++ ) {
+//         fullNames.push(firstNames[i] + lastNames[j]);
+//     }
+// }
+
+// console.log(fullNames);
+
+
+
+
+// -------------------  PRACTICE DONE SUCCESSFULLY ---------------------
